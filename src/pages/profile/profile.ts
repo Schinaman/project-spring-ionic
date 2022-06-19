011471455c9421ddbbcabe3e69455b90bdb7cba6
China <guilherme.hiraoka@gmail.com>
2022-06-19 13:05:24 -0300

    tratando erro 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -31,7 +31,14 @@ export class ProfilePage {
           this.getImageIfExists();
           //buscar img
         },
-        error => {});   
+        error => {
+          if (error.status==403){
+            this.navCtrl.setRoot('HomePage');
+          }
+        });   
+    }
+    else{
+      this.navCtrl.setRoot('HomePage');
     }
     
   }
