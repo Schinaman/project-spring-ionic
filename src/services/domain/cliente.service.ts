fc608e2e54ed2d2816014a678c3a7578308668c3
China <guilherme.hiraoka@gmail.com>
2022-06-19 12:47:08 -0300

    interceptor para header nas requisicoes

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -14,18 +14,14 @@ export class ClienteService {
         }
     
     findBYEmail(email: string): Observable<ClienteDTO>{
-        let token = this.storage.getLocalUser().token;
-        let authHeader = new HttpHeaders({'Authorization': 'Bearer '+token});
         return this.http.get<ClienteDTO>(
-            `${API_CONFIG.baseUrl}/clients/email?value=${email}`,
-            {'headers': authHeader});
+            `${API_CONFIG.baseUrl}/clients/email?value=${email}`);
     }
 
 
     
     getImageFromBucket(id : string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        console.log(this.http.get(url, {responseType : 'blob'}))
         return this.http.get(url, {responseType : 'blob'});
     } 
 }
