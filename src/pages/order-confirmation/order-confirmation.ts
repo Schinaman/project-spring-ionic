1acec21889bde390c62caa51dfef8be8df467d51
China <guilherme.hiraoka@gmail.com>
2022-06-24 22:29:22 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -57,6 +58,7 @@ export class OrderConfirmationPage {
       .subscribe(response => {
         this.cartService.createOrClearCart();
         console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if (error.status ==403){
@@ -66,8 +68,15 @@ export class OrderConfirmationPage {
   }
 
   back(){
-    this.navCtrl.setRoot('CartPage')
+    this.navCtrl.setRoot('CartPage');
+  }
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position+1, location.length);
+  }
 
 }
